cb0206c3a7a4e051d6c00481a6f62cbe92f0606c
billhung <2018@billhung.net>
2020-02-01 18:36:33 +0900

    Randomizer code merged. ランダム処理を実装した, 起動した5秒から実行する

--- GGJ 2020 Repair Shuri Castle/Assets/Scripts/RandomizerController.cs
+++ GGJ 2020 Repair Shuri Castle/Assets/Scripts/RandomizerController.cs
@@ -3,20 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rondomizer : MonoBehaviour
+public class RandomizerController : MonoBehaviour
 {
+    public GameObject obj;
+
     // Start is called before the first frame update
     void Start()
     {
-        foreach (var obj in GetComponentsInChildren(typeof(Renderer)))
-        {
-            obj.transform.localPosition = new Vector3(Random.Range(0, 10), 2, Random.Range(0, 10));
-        }
+
+        StartCoroutine(MyCoroutine());
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator MyCoroutine() {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        foreach (var obj in GetComponentsInChildren(typeof(Renderer))) {
+            obj.transform.localPosition = new Vector3(Random.Range(0, 20), 2, Random.Range(0, 20));
+        }
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
