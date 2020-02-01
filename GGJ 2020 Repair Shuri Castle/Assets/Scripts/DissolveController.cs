29e91d73c3066262c0537660a367f62790a13f87
billhung <2018@billhung.net>
2020-02-01 13:07:33 +0900

    Dissolveの表現処理を追加

---
+++ GGJ 2020 Repair Shuri Castle/Assets/Scripts/DissolveController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+    public bool isDissolving;
+    public float dissolveAmount;
+    public float dissolveSpeed;
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isDissolving = true;
+        }
+
+        if (isDissolving && dissolveAmount > 0) {
+            dissolveAmount -= Time.deltaTime * dissolveSpeed;
+        }
+
+        if (!isDissolving && dissolveAmount < 1) {
+            dissolveAmount += Time.deltaTime * dissolveSpeed;
+        }
+
+        material.SetFloat("_DissolveAmount", dissolveAmount);
+    }
+}
