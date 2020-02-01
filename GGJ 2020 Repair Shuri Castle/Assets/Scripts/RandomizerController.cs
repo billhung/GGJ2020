38046c11938f0e6708fb53790b3ab68557c24cc9
comrojs <cormorant2+github@gmail.com>
2020-02-01 18:10:27 +0900

    Add RandomizerController

---
+++ GGJ 2020 Repair Shuri Castle/Assets/Scripts/RandomizerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rondomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var obj in GetComponentsInChildren(typeof(Renderer)))
+        {
+            obj.transform.localPosition = new Vector3(Random.Range(0, 10), 2, Random.Range(0, 10));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
