eed98128361327f98bf90647adc72a99c1599321
billhung <2018@billhung.net>
2020-02-01 21:37:46 +0900

    Display flashing castle parts if camera is close. カメラをパーツを近づくとピカピカ始める。

--- GGJ 2020 Repair Shuri Castle/Assets/Scripts/DisappearIn5sec.cs
+++ GGJ 2020 Repair Shuri Castle/Assets/Scripts/DisappearIn5sec.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class DisappearIn5sec : MonoBehaviour
 {    
-    //private Renderer gameObj;
-    //private GameObject gameObj;
-
     void Start()
     {
         StartCoroutine(MyCoroutine());
-        //gameObj = this.GetComponent<Renderer>();
-        //gameObj = this.GetComponent<GameObject>();
     }
 
     IEnumerator MyCoroutine() {
         yield return new WaitForSeconds(10);
-        //gameObj.GetComponent<Renderer>().enabled = false;
-        //gameObj.SetActive(false);
-
-        //gameObj.transform.localPosition = new Vector3(0, -1000, 0);
-
+ 
         foreach (var obj in GetComponentsInChildren(typeof(Renderer))) {
-            obj.transform.localPosition = new Vector3(0, -1000, 0);
+            obj.transform.localPosition = new Vector3(obj.transform.localPosition.x, -1000, obj.transform.localPosition.z);
         }
     }
 }
---
+++ GGJ 2020 Repair Shuri Castle/Assets/Scripts/DisplayIfClose.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayIfClose : MonoBehaviour
+{
+    Camera m_MainCamera;
+
+    void Start() {
+        m_MainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        foreach (var obj in GetComponentsInChildren(typeof(Renderer))) {
+            float dist = Vector3.Distance(m_MainCamera.transform.position, 
+                                    new Vector3(obj.transform.localPosition.x, 1,                        obj.transform.localPosition.z));
+            if (dist < 20.0) {
+                obj.transform.localPosition = new Vector3(obj.transform.localPosition.x, 1, obj.transform.localPosition.z);
+            }
+            else {
+                obj.transform.localPosition = new Vector3(obj.transform.localPosition.x, -1000, obj.transform.localPosition.z);
+            }
+            //Debug.Log("dist="+dist+"; Camera<x,y,z>="+ m_MainCamera.transform.position.x+ m_MainCamera.transform.position.y + m_MainCamera.transform.position.z);
+        }
+    }
+}
